1476424da51b659f43f29d7bed4fa355b9948666
Ryan Bezold <Ryan.Bezold@gmail.com>
2025-08-08 21:40:39 -0500

    feat: Display text when no books are found

--- src/app/routes/currently-reading.tsx
+++ src/app/routes/currently-reading.tsx
@@ -1,6 +1,8 @@
 import { fetchBookById } from "@/entities/book";
 import type { Route } from "./+types/currently-reading";
 import { LibraryPage } from "@/pages/library";
+import { Button, Heading, Stack } from "@chakra-ui/react";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,17 +12,40 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function clientLoader({}: Route.LoaderArgs) {
-  const currentlyReading = JSON.parse(localStorage.getItem("currentlyReading") ?? "{}");
+  const currentlyReading = JSON.parse(
+    localStorage.getItem("currentlyReading") ?? "{}"
+  );
 
   const ids = Object.keys(currentlyReading ?? {});
 
   return await Promise.all(
     ids.map((id) => {
-      return fetchBookById(id)
+      return fetchBookById(id);
     })
-  )
+  );
 }
 
-export default function CurrentlyReading({loaderData}: Route.ComponentProps) {
-  return <LibraryPage books={loaderData} />;
+export default function CurrentlyReading({ loaderData }: Route.ComponentProps) {
+  return loaderData.length === 0 ? (
+    _noBooks()
+  ) : (
+    <LibraryPage books={loaderData} />
+  );
+}
+
+function _noBooks() {
+  return (
+    <Stack
+      height={"100%"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      gap={4}
+    >
+      <Heading>You don't have any open books!</Heading>
+      <Heading size={"md"}>Start reading something new!</Heading>
+      <Link to={"/"}>
+        <Button>Go to Library</Button>
+      </Link>
+    </Stack>
+  );
 }
