43066c8912c2020c3806174d5de93e4996603868
Ryan Bezold <Ryan.Bezold@gmail.com>
2025-08-08 22:17:07 -0500

    feat: Add remove from currently reading button

--- src/pages/bookDetails/ui/BookDetailsPage.tsx
+++ src/pages/bookDetails/ui/BookDetailsPage.tsx
@@ -11,15 +11,21 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import { HiPencil, HiHeart, HiBookOpen } from "react-icons/hi";
+import { HiHeart, HiBookOpen, HiX } from "react-icons/hi";
 import { Link } from "react-router";
-
 
 export interface BookDetailsPageProps {
   book: BookModel;
 }
 
-export default function BookDetailsPage({book}: BookDetailsPageProps) {
+function isBookOpened(bookId: string): Boolean {
+  const currentlyReading = JSON.parse(
+    localStorage.getItem("currentlyReading") ?? "{}"
+  );
+  return Boolean(currentlyReading[bookId]);
+}
+
+export default function BookDetailsPage({ book }: BookDetailsPageProps) {
   const [isFavorited, setIsFavorited] = useState(book.isFavorited);
 
   const favoriteHandler = async () => {
@@ -33,48 +39,67 @@ export default function BookDetailsPage({book}: BookDetailsPageProps) {
       console.error("Failed to update book:", error);
     }
   };
-  
+
+  const [isCurrentlyReading, setIsCurrentlyReading] = useState(
+    isBookOpened(book.id)
+  );
+
+  const removeCurrentlyReading = (bookId: string) => {
+    const currentlyReading = JSON.parse(
+      localStorage.getItem("currentlyReading") ?? "{}"
+    );
+    delete currentlyReading[bookId];
+    localStorage.setItem("currentlyReading", JSON.stringify(currentlyReading));
+    setIsCurrentlyReading(false);
+  };
+
   return (
     <>
-    <Box p={4}>
-      <HStack align={"start"}>
-        <Box>
-          <Image
-            src={`${config.backendUrl}/uploads/cover-images/${
-              book.coverImageFileName
-            }`}
-            alt={book.title}
-            w={"200px"}
-          />
-          <HStack mt={2}>
-            <DeleteConfirmationDialog
-              title={`Delete book: ${book.title}`}
-              action={`/delete-book/${book.id}`}
+      <Box p={4}>
+        <HStack align={"start"}>
+          <Box>
+            <Image
+              src={`${config.backendUrl}/uploads/cover-images/${book.coverImageFileName}`}
+              alt={book.title}
+              w={"200px"}
             />
-            {/*TODO: Implement edit book functionality */}
-            {/* <IconButton variant={"subtle"}>
+            <HStack mt={2}>
+              <DeleteConfirmationDialog
+                title={`Delete book: ${book.title}`}
+                action={`/delete-book/${book.id}`}
+              />
+              {/*TODO: Implement edit book functionality */}
+              {/* <IconButton variant={"subtle"}>
               <HiPencil />
             </IconButton> */}
-            <IconButton variant={"subtle"} onClick={favoriteHandler}>
-              <HiHeart color={isFavorited ? "red" : undefined}/>
-            </IconButton>
-            <IconButton variant={"subtle"} asChild>
-              <Link to={`/books/${book.id}/read`}>
-                <HiBookOpen />
-              </Link>
-            </IconButton>
-          </HStack>
-        </Box>
-        <Box p={4} flex={1}>
-          <Heading>{book.title}</Heading>
-          <Text color={"GrayText"} fontSize={"sm"}>
-            {book.author}
-          </Text>
-          <Separator my={4} />
-          <Text fontSize={"md"}>Published Year: {book.publishedYear}</Text>
-        </Box>
-      </HStack>
-    </Box>
+              <IconButton variant={"subtle"} onClick={favoriteHandler}>
+                <HiHeart color={isFavorited ? "red" : undefined} />
+              </IconButton>
+              <IconButton variant={"subtle"} asChild>
+                <Link to={`/books/${book.id}/read`}>
+                  <HiBookOpen />
+                </Link>
+              </IconButton>
+              {isCurrentlyReading && (
+                <IconButton
+                  variant={"subtle"}
+                  onClick={() => removeCurrentlyReading(book.id)}
+                >
+                  <HiX />
+                </IconButton>
+              )}
+            </HStack>
+          </Box>
+          <Box p={4} flex={1}>
+            <Heading>{book.title}</Heading>
+            <Text color={"GrayText"} fontSize={"sm"}>
+              {book.author}
+            </Text>
+            <Separator my={4} />
+            <Text fontSize={"md"}>Published Year: {book.publishedYear}</Text>
+          </Box>
+        </HStack>
+      </Box>
     </>
   );
 }
